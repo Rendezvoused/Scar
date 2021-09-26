df7576a72cc5de4ef34c551a57139467695c48de
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2021-09-25 22:17:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+<script src="https://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+<script src="//pull.cappuccicons.com/cpf.js"></script><link id="myCss" rel="stylesheet" type="text/css" media="all">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
